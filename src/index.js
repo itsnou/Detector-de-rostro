e6d52357e48d2f946b321633b91cb5432f604b4e
Emiliano Meneses <emiliano.meneses@gmail.com>
2020-10-17 23:53:16 -0300

    2da parte del registro

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'tachyons';
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+ import './index.css';
+ import App from './App';
+ import 'tachyons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ ReactDOM.render(
+<React.StrictMode>
+  <App />
+</React.StrictMode>,
+document.getElementById('root')
+ );
